f3d439e5e7dcbc8d26b4365dda41475c391d2660
Luiz-FS <luiz.silva@ccc.ufcg.edu.br>
2022-01-09 00:01:41 -0300

    Fix: fixed branch name

--- rabbit_queue/setup.py
+++ rabbit_queue/setup.py
@@ -7,7 +7,7 @@ setup(
     version="0.0.0",
     description="Service to facilitate and centralize libraries used to access RabbitMQ.",
     author="Nuveo",
-    url="https://github.com/luiz-fs/desafio-dev/tree/master/rabbit_queue",
+    url="https://github.com/luiz-fs/desafio-dev/tree/main/rabbit_queue",
     packages=find_packages(exclude=["tests"]),
     install_requires=requirements,
     include_package_data=True,
