03bba1cd170307ae772e88f718055171055d667f
Luiz-FS <luiz.silva@ccc.ufcg.edu.br>
2022-01-10 20:43:28 -0300

    Feat: created utils module

---
+++ api/cnab/core/utils.py
@@ -0,0 +1,23 @@
+from rabbit_queue.producer import Producer
+from cnab.settings import (
+    BROKER_HOST,
+    BROKER_PORT,
+    BROKER_USER,
+    BROKER_PASS
+)
+
+
+def send_to_worker(data):
+    producer = Producer(
+        broker_host=BROKER_HOST,
+        broker_port=BROKER_PORT,
+        broker_user=BROKER_USER,
+        broker_pass=BROKER_PASS,
+    )
+
+    producer.open_connection()
+    producer.publish(
+        routing_key="file_parser",
+        message=data
+    )
+    producer.close()
