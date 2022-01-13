4be1033b0731109a39000d9921a9d26482a3d1c1
Luiz-FS <luiz.silva@ccc.ufcg.edu.br>
2022-01-13 00:20:43 -0300

    Feat: created App component

---
+++ frontend/src/app/App.js
@@ -0,0 +1,16 @@
+import { Router } from 'react-router-dom';
+import { createBrowserHistory } from 'history';
+import { renderRoutes } from 'react-router-config';
+import routes from '../routes';
+
+const history = createBrowserHistory();
+
+const App = () => {
+  return (
+    <Router history={history}>
+      {renderRoutes(routes)}
+    </Router>
+  );
+}
+
+export default App;
